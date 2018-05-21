bf7eb1e61a1265320855344ae63e42220b46cc7f
Orimol <33389705+Orimol@users.noreply.github.com>
2018-05-21 18:11:12 +0300

    Add files via upload

--- Champion.cpp
+++ Champion.cpp
@@ -8,17 +8,17 @@ const Coordinate Champion::play(const Board& board) {
 //		}
 //
 //	}
-	static Coordinate c{board.size(), board.size()};
-	int counter=board.size();
+	static Coordinate c{board.size, board.size};
+	int counter=board.size;
 	if (board[{0,0}]=='.') return {c};
 	if (board[{0,0}]!='.' && board[{0,1}]=='.' && board[{1,0}]=='.') return c;
 	if(board[{0,0}]!='.' && board[{0,1}]!='.') {
 		counter++;
-		Coordinate c{board.size()-counter, board.size()};
+		Coordinate c{board.size-counter, board.size};
 	}
 	else if(board[{0,0}]!='.' && board[{1,0}]!='.'){
 		counter++;
-		Coordinate c{board.size(),board.size()-counter};
+		Coordinate c{board.size,board.size-counter};
 	}
 	return c;
 
