2eff485e632e3c855a07c2ba5a661600a01420ea
Orimol <33389705+Orimol@users.noreply.github.com>
2018-05-21 19:59:29 +0300

    Add files via upload

--- Champion.h
+++ Champion.h
@@ -4,7 +4,7 @@
 /*
 	This player should win the XYPlayer and YXPlayer asstratagy
 */
-class Champion: public Player {
+const class Champion: public Player {
 public:
 	const string name() const override { return "Ori&Yoav"; }
 	const Coordinate play(const Board& board) override;
