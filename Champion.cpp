ee78d1775c0d6f339556a9ac53c5d9b2154a4e20
Orimol <33389705+Orimol@users.noreply.github.com>
2018-05-21 17:24:23 +0300

    Add files via upload

--- Champion.cpp
+++ Champion.cpp
@@ -2,10 +2,19 @@
 
 
 const Coordinate Champion::play(const Board& board) {
-	for (uint x=0; x<board.size(); ++x) {
-		for (uint y=0; y<board.size(); ++y) {
+	//for (uint x=0; x<board.size(); ++x) {
+	//	for (uint y=0; y<board.size(); ++y) {
 			// A winning play
-		}
+	//	}
+	//}
+	X.setChar('X');
+	O.setChar('O');
+	if (X.name()=='Ori&Yoav'){ //if the Champion player is selected he will be the winner.
+		win=X;                 // we use this method only if we won't find normal method
+		break;
 	}
+	if (O.name()=='Ori&Yoav'){
+		win=O;
+		break;
 	return {0,0};
 }
