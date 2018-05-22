5ecec965de928380721ef7e7f015849d5f47890a
yoavhenig <33389702+yoavhenig@users.noreply.github.com>
2018-05-22 21:36:00 +0300

    Add files via upload

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -5,18 +5,18 @@ void TicTacToe:: play(Player& X, Player& O){
 		win= nullptr;
 		X.setChar('X');
 		O.setChar('O');
-		if (X.name()=='Ori&Yoav'){ //if the Champion player is selected he will be the winner.
+		/*if (X.name()=='Ori&Yoav'){ //if the Champion player is selected he will be the winner.
 			win=X;                 // we use this method only if we won't find normal method
 		}
 		if (O.name()=='Ori&Yoav'){
 			win=O;
-		}
+		}*/
 		if (win== nullptr) {
-			for (int i = 0; i < (size + 1) / 2; i++) {
+			for (int i = 0; i < 320000; i++) {
 				turn(X, O);
-				if ((winner.getChar == 'X') || (winner.getChar == 'O')) break;
+				if ((winner().getChar() == 'X') || (winner().getChar() == 'O')) break;
 				turn(O, X);
-				if ((winner.getChar == 'X') || (winner.getChar == 'O')) break;
+				if ((winner().getChar() == 'X') || (winner().getChar() == 'O')) break;
 			}
 		}
 };
@@ -30,7 +30,7 @@ const Player& TicTacToe::winner()const{
 }
 
 void TicTacToe:: turn(Player& first, Player& second) {
-  /*check 3 things: 1. The place that returns from 'play' method is ligal.
+  /*check 3 things: 1. The place that returns from 'play' method is llegal.
                     2. The place that returns is empty
                     3. Check if the move was a winning move*/
 
@@ -39,7 +39,7 @@ void TicTacToe:: turn(Player& first, Player& second) {
 		try{
 			place = first.play(gameBoard);
 		}
-    catch(/*need to add parameter*/){
+    catch(/*need to add parameter*/...){
 			win = &second;
 		}
 
@@ -62,30 +62,30 @@ void TicTacToe:: turn(Player& first, Player& second) {
   	}
    	//the next two functions checks if the last player won, should be checked after each and every turn.
 	bool checkRow(size_t row, char c){
-		for(int i=0; i<TicTacToe::gameBoard.size; i++){
-			if(TicTacToe::gameBoard[{row,i}]!= c) return false;
+		for(int i=0; i<gameBoard.size(); i++){
+			if(gameBoard[{row,i}]!= c) return false;
 		}
 		return true;
 	}
 
 	bool checkCol(size_t col, char c){
-		for(int i=0; i<TicTacToe::gameBoard.size; i++){
-			if(TicTacToe::gameBoard[{i,col}]!= c) return false;
+		for(int i=0; i<gameBoard.size(); i++){
+			if(gameBoard[{i,col}]!= c) return false;
 		}
 		return true;
 	}
 
 	//the next two functions checks if the last player won diagnoly, should be checked after each and every turn.
 	bool checkDiagnol_topleft(char c){
-		for (int i=0; i<TicTacToe:: gameBoard.size; i++){
-			if(TicTacToe::gameBoard[{i,i}]!= c) return false;
+		for (int i=0; i<gameBoard.size(); i++){
+			if(gameBoard[{i,i}]!= c) return false;
 		}
 		return true;
 	}
 
 	bool checkDiagnol_topright(char c){
-		for (int i=TicTacToe:: gameBoard.size; i>0; i--){
-			if(TicTacToe::gameBoard[{i,i}]!= c) return false;
+		for (int i=gameBoard.size(); i>0; i--){
+			if(gameBoard[{i,i}]!= c) return false;
 		}
 		return true;
 	}
